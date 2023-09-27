07b47ad1fb09569270b00617e1cd5ccd246e1637
Ozhodeda <oz36002@gmail.com>
2023-09-27 12:19:42 +0300

    C/AppHeader/create-app-header

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import './assets/style/main.scss';
 import { Route, HashRouter as Router, Routes } from 'react-router-dom'
 
+import { AppHeader } from './cmp/AppHeader';
+
 export function App() {
   return (
     <Router>
       <section className='main-layout'>
         <AppHeader />
-        <main>
+        {/* <main>
           <Routes>
             <Route element={<HomePage />} path="/" />
             <Route element={<AboutPage />} path="/about" />
             <Route element={<ContactPage />} path="/contact" />
           </Routes>
         </main>
-        <AppFooter />
+        <AppFooter /> */}
       </section>
     </Router>
   )
